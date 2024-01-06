ee8bcfeb7d22880005e1f230f4ada04f6f8c77e6
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-06 15:11:58 -0500

    progress staged
    partial logic for bipolar handle (image dynamic)

--- javascript/script.js
+++ javascript/script.js
@@ -5,8 +5,28 @@ const activeImage =  document.querySelector('.background-image');
 
 dynamicImage.addEventListener('mousemove', position_y);
 
-function position_y(e){ // 0 - 745
+function position_y(e){ // 0 - 745,  => (50.048 - 65.024)
 
-activeImage.style.cssText = `
-background-position: 50% ${e.offsetY / 7.5}%;`
+let baseline = 60;
+let mod = 50 + e.offsetY / 20.5;
+
+// console.log(mod);
+// activeImage.style.cssText = `
+// background-position: 50% ${50 + e.offsetY / 20.5}%;`
+
+if(mod >= 60) {
+setInterval(()  => {
+console.log(`pos == ${baseline}`)
+
+while(baseline < 65) {
+baseline += 1;
+}},2000);}
+
+if(mod <= 60) {
+setInterval(()  => {
+console.log(`neg == ${baseline}`)
+
+while(baseline > 50) {
+baseline -= 1;
+}},2000);}
 }
