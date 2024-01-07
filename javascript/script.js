7c879b943f9ccf73cfad189b3609795a0aa5538d
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-06 20:08:10 -0500

    updated animation logic

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,5 @@
- // 0 - 745,  => (50.048 - 65.024)
+
+
 const dynamicImage = document.querySelector('.overlay');
 const activeImage =  document.querySelector('.background-image');
 
@@ -7,13 +8,16 @@ let someValue = 0;
 
 dynamicImage.addEventListener('mousemove', position_y);
 
-function position_y(e) {
+function position_y(e) { // 0 - 745,  => (50.048 - 65.024)
 let mod = 50 + e.offsetY / 20.5;
 
 intervalId = setInterval(() => {
 someValue += 1;
+console.log(someValue);    
 
-// logic
+if(someValue === 10) {
+clearInterval(intervalId);
+}
 
 },50)
 dynamicImage.removeEventListener('mousemove', position_y);
