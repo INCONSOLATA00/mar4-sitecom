8228e900db4d8df3f6e77b8f704d58451011136e
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-06 20:21:00 -0500

    added additional buffer functionality

--- javascript/script.js
+++ javascript/script.js
@@ -4,21 +4,28 @@ const dynamicImage = document.querySelector('.overlay');
 const activeImage =  document.querySelector('.background-image');
 
 let intervalId;
+let timeoutId;
+
 let someValue = 0;
 
 dynamicImage.addEventListener('mousemove', position_y);
 
 function position_y(e) { // 0 - 745,  => (50.048 - 65.024)
 let mod = 50 + e.offsetY / 20.5;
+console.log(mod);
 
 intervalId = setInterval(() => {
 someValue += 1;
 console.log(someValue);    
 
-if(someValue === 10) {
+if(someValue == 10) {
 clearInterval(intervalId);
+someValue = 0;
 }
 
 },50)
 dynamicImage.removeEventListener('mousemove', position_y);
+timeoutId = setTimeout(() => {dynamicImage.addEventListener('mousemove', position_y);
+clearTimeout(timeoutId);
+},2000)
 }
