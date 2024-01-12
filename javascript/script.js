273c52a49bf1948507263002250ceb29014ddda5
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-12 14:44:14 -0500

    progress staged

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,91 @@
-
-const listen_area = document.querySelector('.image-wrapper')
 const also_background_image = document.querySelector('.also-background-image')
 const background_image =  document.querySelector('.background-image');
 
 
-also_background_image.style.cssText =
-`background-position: ${40}% ${60}%;`
+let x = 0;
+let y = 0;
+
+
+(function() {
+
+function recursion00() {
 
 background_image.style.cssText =
-`background-position: ${40}% ${60}%;`
+`background-position: ${x / 70}% ${y / 170}%;`
 
-
-
-
-
-listen_area.addEventListener('mousemove', position00);
-
-function position00(e) {
 also_background_image.style.cssText =
-`background-position: ${40 - (e.offsetX *-1) / 80}% ${60 - (e.offsetY *-1)  / 80}%;`
-    
-background_image.style.cssText =
-`background-position: ${40 - (e.offsetX *-1) / 80}% ${60 - (e.offsetY *-1)  / 80}%;`
+`background-position: ${x / 170}% ${y / 170}%;`
+
+setTimeout(() =>{
+
+if(x < 2000) {
+x += 1;
+recursion00();
+} else if(x == 2000) {
+alsoRecursion00();
 }
 
+;}, 10)
+}
 
+recursion00();
 
+function alsoRecursion00() {
 
+background_image.style.cssText =
+`background-position: ${x / 170}% ${y / 170}%;`
+
+also_background_image.style.cssText =
+`background-position: ${x / 170}% ${y / 170}%;`
+    
+setTimeout(() =>{
+    
+if(x > 0) {
+x -= 1;
+alsoRecursion00();
+} else if(x == 0) {
+x = 0;
+recursion00();
+}
+    
+;}, 10)
+}
+
+})();
+
+// ------------------------- delay final invocation call for offset / see random
+
+(function() {
+
+function recursion01() {
+    
+setTimeout(() =>{
+    
+if(y < 1000) {
+y += 1;
+recursion01();
+} else if(y == 1000) {
+alsoRecursion01();
+}
+    
+;}, 10)
+}
+    
+recursion01();
+    
+function alsoRecursion01() {
+        
+setTimeout(() =>{
+        
+if(y > 0) {
+y -= 1;
+alsoRecursion01();
+} else if(y == 0) {
+y = 0;
+recursion01();
+}
+        
+;}, 10)
+}
+    
+})();
