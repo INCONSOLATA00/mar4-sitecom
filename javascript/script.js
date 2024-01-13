47a427119878de7a7e6610d43f8a01861afdde1a
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-13 15:21:02 -0500

    *

--- javascript/script.js
+++ javascript/script.js
@@ -11,10 +11,10 @@ let y = 0;
 function recursion00() {
 
 background_image.style.cssText =
-`background-position: ${(-4000 + x) / 10}px ${(-7700 + y) / 10}px;`
+`background-position: ${(-4000 + x) / 19}px ${(-12700 + y) / 19}px;`
 
 also_background_image.style.cssText =
-`background-position: ${(-2000 + x) / 10}px ${(-8700 + y) / 10}px;`
+`background-position: ${(-2000 + x) / 15}px ${(-12700 + y) / 15}px;`
 
 setTimeout(() =>{
 
@@ -33,9 +33,9 @@ recursion00();
 function alsoRecursion00() {
 
 background_image.style.cssText =
-`background-position: ${(-4000 + x) / 10}px ${(-7700 + y) / 10}px;`
+`background-position: ${(-4000 + x) / 19}px ${(-12700 + y) / 19}px;`
 also_background_image.style.cssText =
-`background-position: ${(-2000 + x) / 10}px ${(-8700 + y) / 10}px;`
+`background-position: ${(-2000 + x) / 15}px ${(-12700 + y) / 15}px;`
     
 setTimeout(() =>{
     
