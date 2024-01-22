f2697ade97ac5a739588c4b71868505396d4dc9b
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-21 20:23:55 -0500

    updated stylesheet

--- gallery/javascript/script.js
+++ gallery/javascript/script.js
@@ -1,15 +1,21 @@
 const gallery_images = document.querySelectorAll('img');
+const image_body = document.querySelector('main > div')
 
 let image_index = 0;
+
 gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
 
-function navigate_right(){ 
+image_body.style.cssText = `background-image: url("${gallery_images[0].src}");`
 
+function navigate_right(){ 
 if(image_index > -1 && image_index < 7) {
 
 image_index += 1;
 console.log(image_index);
+
+console.log(gallery_images[image_index].src);
+image_body.style.cssText = `background-image: url("${gallery_images[image_index].src}");`
 
 gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
@@ -19,11 +25,13 @@ gallery_images[image_index - 1].style.cssText =
 };
 
 function navigate_left(){ 
-
 if(image_index > 0 && image_index < 9) {
     
 image_index -= 1;
 console.log(image_index);
+
+console.log(gallery_images[image_index].src);
+image_body.style.cssText = `background-image: url("${gallery_images[image_index].src}");`
     
 gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
