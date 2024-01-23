62e734f78fe8da4a18e546ce8772447dfd9c8159
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-23 04:14:07 -0500

    :C

--- gallery/javascript/script.js
+++ gallery/javascript/script.js
@@ -1,34 +1,42 @@
 const gallery_images = document.querySelectorAll('img');
 const image_body = document.querySelector('main > div');
 
+const end_exe = setInterval(_also, 10);
 
 let i = 0;
 let image_index = 0;
-
+let last_index = image_index;
 
 gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
 
 image_body.style.cssText = `background-image: url("${gallery_images[0].src}");`
 
-function navigate_right(){ 
+    // -----------------------------------
+
+function navigate_right(){
+console.log(`image index ${image_index}`);
+console.log(`last index ${last_index}`);
+
 if(image_index > -1 && image_index < 7) {
 
 image_index += 1;
 dynamic_interval();
+end_exe();
 
 gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
 gallery_images[image_index - 1].style.cssText =
 `border-radius: 0px;`
-}
-};  // -----------------------------------
+}};
+
 
 function navigate_left(){ 
 if(image_index > 0 && image_index < 9) {
     
 image_index -= 1;
 dynamic_interval();
+end_exe();
 
 gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
@@ -37,15 +45,40 @@ gallery_images[image_index + 1].style.cssText =
 }
 };
 
+    // -----------------------------------
+
+if(image_index == 1) {
+function _also() {
+
 (function(){
-
-const end_exe = setInterval(()=>{
-    console.log('running00');
-    image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-    background-image: url("${gallery_images[image_index].src}"), url("${gallery_images[image_index + 1].src}");` // left
-    },10);
-
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
+background-image: url("${gallery_images[0].src}"), url("${gallery_images[image_index].src}");` // initializer***
+last_index = image_index;
 })();
+}};
+
+    
+if(image_index > last_index && image_index % 2 == 0) {
+function _also() {
+
+console.log('runs');
+console.log(image_index);
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index].src}"), url("${gallery_images[image_index + 1].src}");`
+last_index = image_index;
+}};
+
+if (image_index > last_index && image_index % 2 !== 0) {
+function _also() {
+
+console.log('also runs');
+console.log(image_index);
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index].src}"), url("${gallery_images[image_index + 1].src}");`
+last_index = image_index;
+}};
+
+    // -----------------------------------
 
 function dynamic_interval(multiplier = 1){
 setTimeout(()=>{
@@ -55,12 +88,8 @@ i -= 2;
 dynamic_interval(i);
 
 } else if(i <= -428) {
-console.log('executed')
-
 clearInterval(end_exe);
-}
-}, multiplier * (multiplier / 8000));}
+}}, multiplier * (multiplier / 8000));}
 
-// recursion re-initializes interval***
 
 
