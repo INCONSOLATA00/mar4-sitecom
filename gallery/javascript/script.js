7d66ff2eaa4493bd1de26b9198bd9e7664061a10
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-24 07:45:46 -0500

    progress staged

--- gallery/javascript/script.js
+++ gallery/javascript/script.js
@@ -1,32 +1,25 @@
 const gallery_images = document.querySelectorAll('img');
 const image_body = document.querySelector('main > div');
 
+const left_button = document.getElementsByClassName('left')[0];
+const right_button = document.getElementsByClassName('right')[0];
+
+left_button.addEventListener('click', navigate_left);
+right_button.addEventListener('click', navigate_right);
 
 let i = 0;
 let image_index = 0;
-let last_index = image_index;
-
+let disable_other_button = false;
 
 gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
 
 image_body.style.cssText = `background-image: url("${gallery_images[0].src}");`
 
-    // -----------------------------------
 
-function navigate_right(){
-if(image_index > -1 && image_index < 7) {
-
-image_index += 1;
-dynamic_interval();
-
-gallery_images[image_index].style.cssText =
-`border-radius: 10px;`
-gallery_images[image_index - 1].style.cssText =
-`border-radius: 0px;`
-}};
-
+function navigate_left(){ console.log(image_index);
 if(image_index > 0 && image_index < 9) {
+left_button.removeEventListener('click', navigate_left);
     
 image_index -= 1;
 dynamic_interval();
@@ -35,51 +28,95 @@ gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
 gallery_images[image_index + 1].style.cssText =
 `border-radius: 0px;`
-};
+}};
 
-    // -----------------------------------
+
+function navigate_right(){
+if(image_index > -1 && image_index < 7) {
+right_button.removeEventListener('click', navigate_right);
+    
+image_index += 1;
+dynamic_interval();
+    
+gallery_images[image_index].style.cssText =
+`border-radius: 10px;`
+gallery_images[image_index - 1].style.cssText =
+`border-radius: 0px;`
+}};
+
+
+
+(function(){
+image_body.style.cssText = `background-image: url("${gallery_images[0].src}"), url("${gallery_images[image_index].src}");`
+})();
 
 function run_animation(){
-if(image_index == 1) {
 
+switch(image_index) {
+case 1:
 image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[0].src}"), url("${gallery_images[image_index].src}");`
+background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
-};
+break;
 
-    //
-
-if(image_index > last_index && image_index % 2 == 0) { console.log('runs');
-
+case 2:
 image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index].src}"), url("${gallery_images[image_index + 1].src}");`
+background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
-}
+break;
 
-    //
-
-if (image_index > last_index && image_index % 2 !== 0) {
-
+case 3:
 image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index].src}"), url("${gallery_images[image_index + 1].src}");`
+background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
-}
-};
+break;
 
-    // -----------------------------------
+case 4:
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+break;
+
+case 5:
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+break;
+
+case 6:
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+break;
+
+case 7:
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+break;
+
+default:
+}};
+
 
 function dynamic_interval(multiplier = 1){
 setTimeout(()=>{
 if(i < 429 && i > -429) {
 
+disable_other_button = true;
 dynamic_interval(i);
 run_animation();
-i -= 2;
+i -= 1;
 
-// } else if(i <= -428) {
+left_button.removeEventListener('click', navigate_left);
+right_button.removeEventListener('click', navigate_right);
+} else if(i <= -428) {
 
-console.log(i);
-}}, multiplier * (multiplier / 8000));}
+i = 0;
+disable_other_button = false;
+left_button.addEventListener('click', navigate_left);
+right_button.addEventListener('click', navigate_right);
+}}, multiplier * (multiplier / 16000));}
 
 
 
