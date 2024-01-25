885ac98cb475b741b2daf9744d1091cddb16247b
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-25 05:26:13 -0500

    updated logic

--- gallery/javascript/script.js
+++ gallery/javascript/script.js
@@ -9,7 +9,12 @@ right_button.addEventListener('click', navigate_right);
 
 let i = 0;
 let image_index = 0;
+let image_position;
 let disable_other_button = false;
+
+let navigateRight = false;
+let navigateLeft = false;
+
 
 gallery_images[image_index].style.cssText =
 `border-radius: 10px;`
@@ -18,9 +23,12 @@ image_body.style.cssText = `background-image: url("${gallery_images[0].src}");`
 
 
 function navigate_left(){ console.log(image_index);
+navigateLeft = true;
+
 if(image_index > 0 && image_index < 9) {
 left_button.removeEventListener('click', navigate_left);
-    
+
+image_position = -858;
 image_index -= 1;
 dynamic_interval();
 
@@ -32,9 +40,12 @@ gallery_images[image_index + 1].style.cssText =
 
 
 function navigate_right(){
+navigateRight = true;
+
 if(image_index > -1 && image_index < 7) {
 right_button.removeEventListener('click', navigate_right);
-    
+ 
+image_position = 858;
 image_index += 1;
 dynamic_interval();
     
@@ -54,45 +65,101 @@ function run_animation(){
 
 switch(image_index) {
 case 1:
-image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+if(navigateRight == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index -1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
+}
+
+if(navigateLeft == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index +1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+}
 break;
 
 case 2:
-image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+if(navigateRight == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index -1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
+}
+
+if(navigateLeft == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index +1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+}
 break;
 
 case 3:
-image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+if(navigateRight == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index -1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
+}
+
+if(navigateLeft == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index +1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+}
 break;
 
 case 4:
-image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+if(navigateRight == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index -1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
+}
+
+if(navigateLeft == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index +1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+}
 break;
 
 case 5:
-image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+if(navigateRight == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index -1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
+}
+
+if(navigateLeft == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index +1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+}
 break;
 
 case 6:
-image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+if(navigateRight == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index -1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
+}
+
+if(navigateLeft == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index +1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+}
 break;
 
 case 7:
-image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${858 + i * 2}px ${0}px;
-background-image: url("${gallery_images[image_index - 1].src}"), url("${gallery_images[image_index].src}");`
+if(navigateRight == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index -1].src}"), url("${gallery_images[image_index].src}");`
 last_index = image_index;
+}
+
+if(navigateLeft == true) {
+image_body.style.cssText = `background-position: ${i * 2}px ${0}px, ${image_position + i * 2}px ${0}px;
+background-image: url("${gallery_images[image_index +1].src}"), url("${gallery_images[image_index].src}");`
+last_index = image_index;
+}
 break;
 
 default:
@@ -106,14 +173,23 @@ if(i < 429 && i > -429) {
 disable_other_button = true;
 dynamic_interval(i);
 run_animation();
-i -= 1;
+
+if(navigateLeft == true) {
+i += 1.5;}
+
+if(navigateRight == true) {
+i -= 1.5;}
 
 left_button.removeEventListener('click', navigate_left);
 right_button.removeEventListener('click', navigate_right);
 } else if(i <= -428) {
 
 i = 0;
+
 disable_other_button = false;
+navigateLeft = false;
+navigateRight = false;
+
 left_button.addEventListener('click', navigate_left);
 right_button.addEventListener('click', navigate_right);
 }}, multiplier * (multiplier / 16000));}
