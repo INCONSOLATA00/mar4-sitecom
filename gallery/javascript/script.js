07667677888c420dbe0970a19ef1e5abbaa1a31f
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-01-31 04:26:55 -0500

    progress staged

--- gallery/javascript/script.js
+++ gallery/javascript/script.js
@@ -10,7 +10,7 @@ right_button.addEventListener('click', button_used_right);
 
 let i = 1;
 let image_index = 0;
-let last_index = image_index;
+let last_index;
 
 let image_position;
 let disable_other_button = false;
@@ -28,39 +28,34 @@ if(j < 10) {
 animating = true;
 j += 0.5;
 fader(j);
+}}, multiplier * multiplier * (multiplier / 6));}
 
-
-}}, multiplier * multiplier * (multiplier / 6));
-}
 
 
 for(let i = 0; i < gallery_images.length; i++) {
 gallery_images[i].addEventListener('click', gallery_select00);
 gallery_images[i].id = i;}
 
-function mouse_used(){ buttonUsed = false;}
 
+function mouse_used(){ buttonUsed = false;}
 function button_used_left(){ 
 buttonUsed = true;
 navigate_left();}
-
 function button_used_right(){ 
 buttonUsed = true;
 navigate_right();}
 
-    // --------------------------
+
 
 let last_image_index;
 let first_image_index;
-
 let initializer = false;
 
-
-function gallery_select00(e){ // CURRENT
+function gallery_select00(e){
 mouse_used();
-
 last_image_index = first_image_index;
 first_image_index = e.target.id;
+
 
 if(first_image_index > last_image_index && first_image_index < 7 && initializer == true){ console.log('greater00');
 image_index = +first_image_index;
@@ -71,7 +66,21 @@ navigate_right();
 image_index = 7;
 navigateRight = true;
 navigate_right();
-}
+
+if(j < 10){  console.log('runs01');
+customInterval00();}
+
+fader();
+function customInterval00 (){
+if(j < 10) {
+setTimeout(() =>{
+document.getElementById(image_index).style.cssText = 
+`border-radius: ${j}px`;
+customInterval00();
+},1);
+}}}
+
+
 
 if(initializer == false){ console.log('initial value');
 document.getElementById(0).style.cssText = 
@@ -81,11 +90,9 @@ initializer = true;
 image_index = +first_image_index;
 navigateRight = true;
 navigate_right();
+}
 
-}else if(first_image_index !== last_image_index){
-j = 0; // start radius animation
-document.getElementById(last_image_index).style.cssText = 
-`border-radius: 0px`;}
+
 
 if(first_image_index < last_image_index && first_image_index > 0){ console.log('lesser00');  
 image_index = +first_image_index;
@@ -96,35 +103,29 @@ navigate_left();
 image_index = 0;
 navigateLeft = true;
 navigate_left();
-}
-    // if first_image_index - 1, is less than 0, call navigate left with no assignment
-    // --------------------------
 
-if(j < 10){
+if(j < 10){  console.log('runs01'); // j may need to be set to 0
 customInterval00();}
 
-reset();
 fader();
 function customInterval00 (){
 if(j < 10) {
 setTimeout(() =>{
-document.getElementById(first_image_index).style.cssText = 
+document.getElementById(image_index).style.cssText = 
 `border-radius: ${j}px`;
 customInterval00();
 },1);
-}}};
+}}
+}};
 
 
-function reset(){
-gallery_images[image_index].style.cssText =
-`border-radius: 0px;`
-}
-gallery_images[image_index].style.cssText =
-`border-radius: 10px;`
-
-image_body.style.cssText = `background-image: url("${gallery_images[0].src}");`
 
 function navigate_left(){
+for(i = 0; i < gallery_images.length; i++){
+gallery_images[i].style.cssText =
+`border-radius: 0px;`}
+last_index = image_index;
+
 if(image_index > 0 || first_image_index == 0) {
 
 navigateLeft = true;
@@ -134,22 +135,38 @@ image_position = -858;
 if(buttonUsed == true) {
 image_index--;
 
-gallery_images[image_index].style.cssText =
-`border-radius: 10px;`
-gallery_images[image_index + 1].style.cssText =
+if(image_index !== last_index){
+gallery_images[last_index].style.cssText =
 `border-radius: 0px;`
-dynamic_interval();
+dynamic_interval();}}
 
-}
 if (buttonUsed == false) { console.log('navigate left');
-dynamic_interval();
-};}};
+dynamic_interval();};}
+
+if(j < 10){ console.log('runs00');
+customInterval00();}
+
+fader();
+function customInterval00 (){
+if(j < 10 && image_index > 0) {
+setTimeout(() =>{
+document.getElementById(image_index).style.cssText = 
+`border-radius: ${j}px`;
+customInterval00();
+},1);
+}}};
 
 
-function navigate_right(){
+
+function navigate_right(){ 
+for(i = 0; i < gallery_images.length; i++){
+gallery_images[i].style.cssText =
+`border-radius: 0px;`}
+
+last_index = image_index;
+
 if(first_image_index == 0) {image_index ++;}
 if(image_index < 7 || first_image_index == 7) {
-
 
 navigateRight = true;
 right_button.removeEventListener('click', navigate_right);
@@ -158,24 +175,37 @@ image_position = 858;
 if(buttonUsed == true) {
 image_index++;
 
-gallery_images[image_index].style.cssText =
-`border-radius: 10px;`
-gallery_images[image_index - 1].style.cssText =
+if(image_index !== last_index){
+gallery_images[last_index].style.cssText =
 `border-radius: 0px;`
-dynamic_interval();
+dynamic_interval();}}
 
-}
 if (buttonUsed == false) { console.log('navigate right');
-dynamic_interval();
-};}};
+dynamic_interval();};}
+
+if(j < 10){  console.log('runs01');
+customInterval00();}
+
+fader();
+function customInterval00 (){
+if(j < 10 && image_index < 7) {
+setTimeout(() =>{
+document.getElementById(image_index).style.cssText = 
+`border-radius: ${j}px`;
+customInterval00();
+},1);
+}}};
 
 
 
 (function(){
+gallery_images[0].style.cssText =
+`border-radius: 10px;`
+
 image_body.style.cssText = `background-image: url("${gallery_images[0].src}"), url("${gallery_images[image_index].src}");`
 })();
 
-function run_animation(){ console.log(image_index);
+function run_animation(){
 switch(image_index) { 
 case 0:
 if(navigateRight == true) { // all values are true, but statement won't execute
@@ -273,18 +303,19 @@ background-image: url("${gallery_images[image_index +1].src}"), url("${gallery_i
 }
 break;
 
-default:
+// default:
 }};
 
 
-function dynamic_interval(){ // later remove mouse event***
+
+
+function dynamic_interval(){
 setTimeout(() =>{
 if(i < 429 && i > -430) {
 
 disable_other_button = true;
 dynamic_interval(i);
 run_animation();
-
 
 
 if(navigateLeft == true) {
@@ -295,13 +326,15 @@ i -= 1.5;}
 
 left_button.removeEventListener('click', button_used_left);
 right_button.removeEventListener('click', button_used_right);
-
 for(let i = 0; i < gallery_images.length; i++) {
 gallery_images[i].removeEventListener('click', gallery_select00);}
+
+
 
 } else if(i <= 1 - image_position / 2) {
 
 i = 1;
+j = 0;
 disable_other_button = false;
 navigateLeft = false;
 navigateRight = false;
