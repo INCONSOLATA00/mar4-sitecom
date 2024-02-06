cef2fd83c2664b0f85c09b9ce74b44bd2b4ab56d
INCONSOLATA00 <gabe.rattigan@gmail.com>
2024-02-06 18:16:31 -0500

    added image info, fixed a minor bug

--- gallery/javascript/script.js
+++ gallery/javascript/script.js
@@ -1,8 +1,21 @@
 const gallery_images = document.querySelectorAll('img');
 const image_body = document.querySelector('main > div');
+const image_info = document.querySelector('.image-info-wrapper + div > p')
 
 const left_button = document.getElementsByClassName('left')[0];
 const right_button = document.getElementsByClassName('right')[0];
+
+const gallery_text = [
+
+{ text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit elit eros, suscipit vestibulum libero dictum ut.(1)" },
+{ text: "Duis lobortis sem nec sem bibendum, a sollicitudin nulla pretium. Mauris in orci eget neque posuere fermentum. Suspendisse ultrices posuere dignissim. Sed id ex risus.(2)" },
+{ text: "Morbi egestas ex et urna pellentesque, nec euismod tellus pellentesque. Integer venenatis libero vel diam sodales, sit amet pretium massa pretium. Duis vel dui vel eros condimentum sagittis.(3)" },
+{ text: "Vestibulum quis gravida lacus. Mauris euismod mattis dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.(4)" },
+{ text: "Maecenas efficitur lacus vel nulla pretium, sed feugiat leo interdum.(5)" },
+{ text: "Etiam consequat sem vitae orci volutpat consectetur. Vestibulum viverra eros a iaculis bibendum.(6)" },
+{ text: "Curabitur rhoncus feugiat nisl, a malesuada nisi molestie sit amet. Donec tincidunt quis justo sit amet pulvinar.(7)" },
+{ text: "Mauris a nisl vel urna ultricies sollicitudin. Vivamus fringilla leo at odio lobortis, a vulputate mauris lobortis. Ut accumsan justo vel dui convallis, pharetra aliquam ipsum ullamcorper.(8)" },
+];
 
 left_button.addEventListener('click', button_used_left);
 right_button.addEventListener('click', button_used_right);
@@ -22,7 +35,7 @@ let buttonState = true;
 
 let count = 0;
 let value = false;
-let value_index;
+let value_index; // true last index*
 
 let j = 0;
 function fader(multiplier = 1){
@@ -94,14 +107,14 @@ navigateRight = true;
 navigate_right();
 }
 
-if(first_image_index < last_image_index && first_image_index > 0){
+if(first_image_index < last_image_index || first_image_index < image_index && first_image_index > 0){ // || allows conditional while value offset
 
 value_index = image_index;
 image_index = +first_image_index;
 navigateLeft = true;
 navigate_left();
 
-} else if(first_image_index < last_image_index && first_image_index == 0){
+} else if(first_image_index < last_image_index || first_image_index < image_index && first_image_index == 0){ // || allows conditional while value offset
 image_index = 0;
 navigateLeft = true;
 navigate_left();
@@ -119,15 +132,13 @@ customInterval00();
 },1);
 }}}};
 
-function navigate_left(){ console.log(count);
+function navigate_left(){
 
 if(buttonState !== buttonUsed) {
 if(count > 0){
 value = true;
-console.log(value);
 }} else {
 value = false;
-console.log(value);
 };
 
 buttonState = buttonUsed;
@@ -169,15 +180,13 @@ customInterval00();
 },1);
 }}};
 
-function navigate_right(){ console.log(count); // current
+function navigate_right(){
 
 if(buttonState !== buttonUsed) {
 if(count > 0){
 value = true;
-console.log(value);
 }} else {
 value = false;
-console.log(value);
 };
 
 buttonState = buttonUsed;
@@ -437,9 +446,10 @@ navigateRight = false;
 if(image_index > 0) { left_button.addEventListener('click', button_used_left);}
 if(image_index < 7) { right_button.addEventListener('click', button_used_right);}
 
+image_info.textContent = `${gallery_text[image_index].text}`;
+
 for(let i = 0; i < gallery_images.length; i++) {
 gallery_images[i].addEventListener('click', gallery_select00);}
 
 }}, i * (i / 16000));}
 
-// console.log(`image_index: ${image_index} last_index: ${last_index} first_image_index: ${first_image_index} last_image_index: ${last_image_index}`);
--- javascript/script.js
+++ javascript/script.js
@@ -9,12 +9,12 @@ let y = 0;
 let i = 0;
 
 const card_text = [
+
 { title: 'Post Construction Cleaning', text: "Renovations and Construction can be messy. After the work is finished, a thorough cleaning is critical to make the work shine and the space ready for use. Our experienced professionals can help you make the most of your investment." },
 { title: 'Building Maintenance & Renovations', text: "We will be happy to fulfill all of your maintenance, renovation or consturction needs. From things as simple as repairing a door that doesn't close properly right down to a building a whole new space, we have the experience, skills and contacts to do it all and do it right, at a price that won't hurt your wallet." },
 { title: 'Commercial Cleaning', text: "Are you looking for someone to keep your office, store or building clean? We have a great staff of cleaners that are eager to keep your space looking great. Whether you need full service daily or just someone to pop in and do a few specific tasks weekly, we can help create a service plan that suits your needs." },
 { title: 'Construction Deficiency Resolution', text: "Have your contractors finished the project, left the job, all done but left an item or two incomplete or not quite up to your standards? Maybe an aspect of the project didn't turn out the way you expected and you want to make a change?" }
 ];
-
 
 cardTitle.textContent = card_text[i].title;
 card.textContent = card_text[i].text;
